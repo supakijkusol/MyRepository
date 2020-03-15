13ea26f0ba8106f9fdae23b351de23bc9acb6adb
Supakij Kusol <supakij.kusol@gmail.com>
2020-03-15 22:55:40 +0700

    Commit python file from pycharm to github v.1.0.3  - branch 2    - test merge branch to master

--- projects/test_python.py
+++ projects/test_python.py
@@ -1,3 +1,6 @@
 # test create new file from Github
 # test new branch 2
 # commit before merge branch to master 4
+# ====
+# this is code was develop by programmer B
+# reviewed by programmer A and B and accepted code
